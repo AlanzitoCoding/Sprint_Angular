3b3f3868ac1131eb720f6344b81c4918a0518e46
AlanzitoCoding <alanzito006@gmail.com>
2025-11-07 11:49:25 -0300

    refactor: estruturação dos arquivos em pastas e roteamento

---
+++ src/app/shared/menu/menu.component.spec.ts
@@ -0,0 +1,25 @@
+// Louvado seja o Senhor
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MenuComponent } from './menu.component';
+
+describe('MenuComponent', () => {
+  let component: MenuComponent;
+  let fixture: ComponentFixture<MenuComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MenuComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,13 @@
+// Louvado seja o Senhor
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sprAng-menu',
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}
